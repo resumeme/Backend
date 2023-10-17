a54642959d53a718ebc0ae5f913e2aa441164eb0
JuHongseok <wnghdtjr129@naver.com>
2023-10-17 17:53:53 +0900

    feat: project setting

---
+++ src/main/java/org/devcourse/resumeme/ResumemeApplication.java
@@ -0,0 +1,13 @@
+package org.devcourse.resumeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/devcourse/resumeme/ResumemeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devcourse.resumeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
