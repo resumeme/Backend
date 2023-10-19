f65a35d164259d670b29d2b263688a64ad8f1c85
김범우 <86512449+beomukim@users.noreply.github.com>
2023-10-19 14:47:36 +0900

    중복 파일 제거 #7

--- src/main/java/org/devcourse/resumeme/global/config/WebConfig.java
+++ src/main/java/org/devcourse/resumeme/global/config/WebConfig.java
@@ -1,4 +1,4 @@
-package org.devcourse.global.util;
+package org.devcourse.resumeme.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -8,21 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${appConfig.cors.allowedOrigins}")
-    private String[] allowedOrigins;
-
-    @Value("${appConfig.cors.corsMapping}")
-    private String corsMapping;
-
-    @Value("${appConfig.cors.maxAge}")
-    private int maxAge;
-
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping(corsMapping)
-                .allowedOrigins(allowedOrigins)
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-                .maxAge(maxAge);
+                .maxAge(3600);
     }
 
 }
