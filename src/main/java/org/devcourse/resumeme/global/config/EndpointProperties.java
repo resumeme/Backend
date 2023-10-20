fdda84d3399ab4968fdd9f661b262ce9b1efe396
JuHongseok <wnghdtjr129@naver.com>
2023-10-21 00:13:08 +0900

    feat: role 관련 필드 추가

--- src/main/java/org/devcourse/resumeme/global/config/EndpointProperties.java
+++ src/main/java/org/devcourse/resumeme/global/config/EndpointProperties.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
+import java.util.Map;
 
 @ConfigurationProperties("endpoint")
 public class EndpointProperties {
@@ -11,8 +12,12 @@ public class EndpointProperties {
     @Getter
     private List<String> permitAll;
 
-    public EndpointProperties(List<String> permitAll) {
+    @Getter
+    private Map<String, List<String>> roles;
+
+    public EndpointProperties(List<String> permitAll, Map<String, List<String>> roles) {
         this.permitAll = permitAll;
+        this.roles = roles;
     }
 
 }
