972fba707d01843d78dca57d5453d00d92696778
JuHongseok <wnghdtjr129@naver.com>
2023-10-20 23:01:50 +0900

    feat: properties 파일, 객체 생성

--- src/main/java/org/devcourse/resumeme/global/config/WebConfig.java
+++ src/main/java/org/devcourse/resumeme/global/config/WebConfig.java
@@ -1,11 +1,12 @@
 package org.devcourse.resumeme.global.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ConfigurationPropertiesScan
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
