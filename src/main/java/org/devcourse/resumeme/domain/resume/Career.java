60e2fa3ae5ce7813a037cb0ed6903fe66191cd67
bumwoo <gld55602@gmail.com>
2023-10-20 15:01:21 +0900

    feat: 경력 고용형태 추가

--- src/main/java/org/devcourse/resumeme/domain/resume/Career.java
+++ src/main/java/org/devcourse/resumeme/domain/resume/Career.java
@@ -1,11 +1,9 @@
 package org.devcourse.resumeme.domain.resume;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.NoArgsConstructor;
-import org.devcourse.resumeme.common.domain.Position;
 
 import java.time.LocalDate;
 
@@ -16,7 +14,7 @@ public class Career {
     private String companyName;
 
     @Enumerated(EnumType.STRING)
-    private Position careerPosition;
+    private EmploymentType employmentType;
 
     private LocalDate careerStartDate;
 
@@ -24,9 +22,9 @@ public class Career {
 
     private String careerContent;
 
-    public Career(String companyName, Position careerPosition, LocalDate careerStartDate, LocalDate endDate, String careerContent) {
+    public Career(String companyName, EmploymentType employmentType, LocalDate careerStartDate, LocalDate endDate, String careerContent) {
         this.companyName = companyName;
-        this.careerPosition = careerPosition;
+        this.employmentType = employmentType;
         this.careerStartDate = careerStartDate;
         this.endDate = endDate;
         this.careerContent = careerContent;
