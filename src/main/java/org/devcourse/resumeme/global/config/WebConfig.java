0cd3c32c201c8fd8833d32ffb78325e2792f9c1f
bumwoo <gld55602@gmail.com>
2023-10-20 16:37:54 +0900

    cors url 추가

--- src/main/java/org/devcourse/resumeme/global/config/WebConfig.java
+++ src/main/java/org/devcourse/resumeme/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://resumeme.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .maxAge(3600);
     }
