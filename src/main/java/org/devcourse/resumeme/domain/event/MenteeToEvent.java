e109967de977464d327ce1d3e9dcb288a0b4c901
JHS <wnghdtjr129@naver.com>
2023-10-20 16:53:00 +0900

    feat: baseEntity, pk Column name 추가

--- src/main/java/org/devcourse/resumeme/domain/event/MenteeToEvent.java
+++ src/main/java/org/devcourse/resumeme/domain/event/MenteeToEvent.java
@@ -1,20 +1,23 @@
 package org.devcourse.resumeme.domain.event;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.NoArgsConstructor;
+import org.devcourse.resumeme.common.domain.BaseEntity;
 
 import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @NoArgsConstructor
-public class MenteeToEvent {
+public class MenteeToEvent extends BaseEntity {
 
     @Id
     @GeneratedValue
+    @Column(name = "mentee_to_event_id")
     private Long id;
 
     @ManyToOne(fetch = LAZY)
