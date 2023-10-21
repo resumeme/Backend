2ec3d4acef634508a952c1dea5c362e5e64ace50
JHS <wnghdtjr129@naver.com>
2023-10-21 15:37:59 +0900

    feat: 도메인 연관관계 수정

--- src/main/java/org/devcourse/resumeme/domain/event/EventPosition.java
+++ src/main/java/org/devcourse/resumeme/domain/event/EventPosition.java
@@ -2,20 +2,30 @@ package org.devcourse.resumeme.domain.event;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.NoArgsConstructor;
+import org.devcourse.resumeme.common.domain.Position;
 
+import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+import static org.devcourse.resumeme.common.util.Validator.validate;
 
 @Entity
+@NoArgsConstructor(access = PROTECTED)
 public class EventPosition {
 
     @Id
     @GeneratedValue
     @Column(name = "event_position_id")
     private Long id;
+
+    @Enumerated(STRING)
+    private Position position;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "event_id")
