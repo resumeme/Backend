b837ea1c1b15a2d8e36ca882be7d0ab3e1360c31
JuHongseok <wnghdtjr129@naver.com>
2023-10-21 16:57:38 +0900

    test: EventInfo 관련 테스트 작성 및 코드 일부 수정

--- src/main/java/org/devcourse/resumeme/domain/event/EventInfo.java
+++ src/main/java/org/devcourse/resumeme/domain/event/EventInfo.java
@@ -69,7 +69,11 @@ public class EventInfo {
     }
 
     public void open() {
-        this.status = EventStatus.OPEN;
+        if (!status.isBook()) {
+            throw new EventException("CANNOT_OPEN", "예약한 이벤트에 한에서만 오픈 신청읋 할 수 있습니다");
+        }
+
+        status = EventStatus.OPEN;
     }
 
 }
