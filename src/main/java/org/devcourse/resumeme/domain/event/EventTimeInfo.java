48677c8547793e145d8f1ca3ad902c3a72e493ce
JHS <wnghdtjr129@naver.com>
2023-10-21 15:36:20 +0900

    feat: 도메인 validation 로직 추가

--- src/main/java/org/devcourse/resumeme/domain/event/EventTimeInfo.java
+++ src/main/java/org/devcourse/resumeme/domain/event/EventTimeInfo.java
@@ -6,8 +6,11 @@ import org.devcourse.resumeme.domain.event.exception.EventException;
 
 import java.time.LocalDateTime;
 
+import static lombok.AccessLevel.PROTECTED;
+import static org.devcourse.resumeme.common.util.Validator.validate;
+
 @Embeddable
-@NoArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
 public class EventTimeInfo {
 
     private LocalDateTime openDateTime;
@@ -15,8 +18,16 @@ public class EventTimeInfo {
     private LocalDateTime endDate;
 
     private EventTimeInfo(LocalDateTime openDateTime, LocalDateTime endDate) {
+        validateInput(openDateTime, endDate);
+
         this.openDateTime = openDateTime;
         this.endDate = endDate;
+    }
+
+    private void validateInput(LocalDateTime openDateTime, LocalDateTime endDate) {
+        validate(openDateTime == null, "NO_EMPTY_VALUE", "시작 시간은 빈 값일 수 없습니다");
+        validate(endDate == null, "NO_EMPTY_VALUE", "종료 시간은 빈 값일 수 없습니다");
+        validate(openDateTime.isAfter(endDate), "TIME_ERROR", "시작 시간이 종료 시간보다 빨라야 합니다");
     }
 
     public static EventTimeInfo onStart(LocalDateTime endDate) {
