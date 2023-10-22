ffe15449ef390e4bf1367c5bb4c9055f03d22685
ddongpuri <fnffnfkffk1112@gmail.com>
2023-10-22 18:54:21 +0900

    feat : OAuth2UserInfo 인터페이스 및 구체클래스 생성

---
+++ src/main/java/org/devcourse/resumeme/global/auth/OAuth2CustomUserService.java
@@ -0,0 +1,63 @@
+package org.devcourse.resumeme.global.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.devcourse.resumeme.domain.user.Provider;
+import org.devcourse.resumeme.domain.user.Role;
+import org.devcourse.resumeme.domain.user.User;
+import org.devcourse.resumeme.global.auth.userInfo.GoogleOAuth2UserInfo;
+import org.devcourse.resumeme.global.auth.userInfo.KakaoOAuth2UserInfo;
+import org.devcourse.resumeme.global.auth.userInfo.OAuth2UserInfo;
+import org.devcourse.resumeme.repository.UserRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OAuth2CustomUserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId(); // kakao, google
+        Provider socialType = Provider.valueOf(registrationId.toUpperCase());
+        String provider = userRequest.getClientRegistration().getRegistrationId(); // google
+        OAuth2UserInfo userInfo = null;
+
+        if (provider.equals("google")) {
+            userInfo = new GoogleOAuth2UserInfo(oAuth2User.getAttributes());
+        }
+
+        if (provider.equals("kakao")) {
+            userInfo = new KakaoOAuth2UserInfo(oAuth2User.getAttributes());
+        }
+
+        if (userInfo == null) {
+            throw new RuntimeException("지원하지 않는 소셜로그인");
+        }
+
+        String username = provider + "_" + userInfo.getId();
+        User userEntity = userRepository.findByUsername(username);
+
+        if (userEntity == null) { // 최초 로그인
+            userEntity = User.builder()
+                    .username(username)
+                    .imageUrl(userInfo.getImageUrl())
+                    .role(Role.ROLE_GUEST)
+                    .password("resumeme")
+                    .email(userInfo.getEmail())
+                    .provider(socialType)
+                    .build();
+
+            userEntity = userRepository.save(userEntity);
+        }
+
+        return new PrincipalDetails(userEntity, oAuth2User.getAttributes());
+    }
+
+}
---
+++ src/main/java/org/devcourse/resumeme/global/auth/PrincipalDetails.java
@@ -0,0 +1,73 @@
+package org.devcourse.resumeme.global.auth;
+
+import lombok.Data;
+import lombok.Getter;
+import org.devcourse.resumeme.domain.user.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+@Data
+public class PrincipalDetails implements UserDetails, OAuth2User {
+
+    @Getter
+    private User user;
+    private Map<String, Object> attributes;
+
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+        collect.add(()-> String.valueOf(user.getRole()));
+        return collect;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+}
