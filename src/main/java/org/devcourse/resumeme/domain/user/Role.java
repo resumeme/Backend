2dd949bf0102adce9dc2efe72b01980202268c7c
ddongpuri <fnffnfkffk1112@gmail.com>
2023-10-22 19:54:05 +0900

    feat : 요청된 role에 대한 검증 추가

--- src/main/java/org/devcourse/resumeme/domain/user/Role.java
+++ src/main/java/org/devcourse/resumeme/domain/user/Role.java
@@ -6,5 +6,11 @@ public enum Role {
     ROLE_TEMP, // 소셜로그인 후 추가 필수 정보 기입 (멘토) + 아직 멘토 승인 요청 안보낸 상태
     ROLE_PENDING, // 소셜로그인 후 추가 필수 정보 기입 (멘토) + 멘토 승인 요청 보내고 심사 대기중
     ROLE_MENTOR, // 멘토 승인 완료
-    ROLE_ADMIN
+    ROLE_ADMIN;
+
+    public void checkSignUpRequest() {
+        if (this != Role.ROLE_PENDING && this != Role.ROLE_MENTEE) {
+            throw new IllegalArgumentException("잘못된 요청입니다.");
+        }
+    }
 }
