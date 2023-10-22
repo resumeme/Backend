80f0afea62975c080e88a6f1cd6d823a6c7ccb1e
bumwoo <gld55602@gmail.com>
2023-10-22 16:26:44 +0900

    feat: Resume 저장 기능 추가

---
+++ src/main/java/org/devcourse/resumeme/controller/ResumeController.java
@@ -0,0 +1,37 @@
+package org.devcourse.resumeme.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.devcourse.resumeme.controller.dto.ResumeCreateRequest;
+import org.devcourse.resumeme.controller.dto.ResumeCreateResponse;
+import org.devcourse.resumeme.domain.mentee.Mentee;
+import org.devcourse.resumeme.domain.resume.Resume;
+import org.devcourse.resumeme.service.MenteeService;
+import org.devcourse.resumeme.service.ResumeService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/resume")
+public class ResumeController {
+
+    private final ResumeService resumeService;
+
+    private final MenteeService menteeService;
+
+    @PostMapping("/{menteeId}")
+    public ResumeCreateResponse createResume(
+            @PathVariable Long menteeId,
+            @RequestBody ResumeCreateRequest request) {
+
+        Mentee mentee = menteeService.getOne(menteeId);
+        Resume resume = request.toEntity(mentee);
+
+        Long savedId = resumeService.create(resume);
+        return new ResumeCreateResponse("이력서 등록을 성공했습니다.", savedId);
+    }
+
+}
