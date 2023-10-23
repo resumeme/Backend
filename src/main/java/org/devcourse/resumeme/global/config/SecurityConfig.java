50278f62975b9195b9f095363cbde824c7c20006
JuHongseok <wnghdtjr129@naver.com>
2023-10-23 15:37:16 +0900

    fix: endpoint 설정 http method 추가

--- src/main/java/org/devcourse/resumeme/global/config/SecurityConfig.java
+++ src/main/java/org/devcourse/resumeme/global/config/SecurityConfig.java
@@ -6,8 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.List;
 import java.util.Map;
@@ -20,8 +19,6 @@ public class SecurityConfig {
 
     private final EndpointProperties properties;
 
-    private final HandlerMappingIntrospector introspector;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         setEndpoints(http);
@@ -33,18 +30,27 @@ public class SecurityConfig {
     }
 
     private void setEndpoints(HttpSecurity http) throws Exception {
-        for (String endpoint : properties.getPermitAll()) {
-            http.authorizeHttpRequests(registry ->
-                    registry.requestMatchers(new MvcRequestMatcher(introspector, endpoint)).permitAll()
-            );
+        for (Map.Entry<String, List<String>> entry : properties.getPermitAll().entrySet()) {
+            String method = entry.getKey();
+
+            for (String endPoint : entry.getValue()) {
+                http.authorizeHttpRequests(registry ->
+                        registry.requestMatchers(new AntPathRequestMatcher(endPoint, method)).permitAll());
+            }
         }
 
-        for (Map.Entry<String, List<String>> entry : properties.getRoles().entrySet()) {
-            String roleName = entry.getKey();
+        for (EndpointProperties.Matcher role : properties.getRoles()) {
+            String[] roles = role.getRole().stream()
+                    .map(String::toUpperCase)
+                    .toArray(String[]::new);
 
-            for (String endpoint : entry.getValue()) {
-                http.authorizeHttpRequests(registry ->
-                        registry.requestMatchers(new MvcRequestMatcher(introspector, endpoint)).hasRole(roleName.toUpperCase()));
+            for (Map.Entry<String, List<String>> entry : role.getMatcher().entrySet()) {
+                String method = entry.getKey();
+
+                for (String endPoint : entry.getValue()) {
+                    http.authorizeHttpRequests(registry ->
+                            registry.requestMatchers(new AntPathRequestMatcher(endPoint, method)).hasAnyRole(roles));
+                }
             }
         }
     }
