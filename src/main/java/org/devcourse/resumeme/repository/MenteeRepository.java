541ad0b1b8d608b0a9163536a2c41177784e6d16
bumwoo <gld55602@gmail.com>
2023-10-23 14:33:20 +0900

    feat: mentee, mentor 엔티티 제거

--- src/main/java/org/devcourse/resumeme/repository/MenteeRepository.java
+++
@@ -1,8 +0,0 @@
-package org.devcourse.resumeme.repository;
-
-import org.devcourse.resumeme.domain.mentee.Mentee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenteeRepository extends JpaRepository<Mentee, Long> {
-
-}
