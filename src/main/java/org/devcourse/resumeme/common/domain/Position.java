3a33adf633273fbabdd7c7befadf25e5c66e43f3
JuHongseok <wnghdtjr129@naver.com>
2023-10-23 23:31:04 +0900

    chore: 공백 추가

--- src/main/java/org/devcourse/resumeme/common/domain/Position.java
+++ src/main/java/org/devcourse/resumeme/common/domain/Position.java
@@ -1,9 +1,9 @@
 package org.devcourse.resumeme.common.domain;
 
-public enum Position implements DocsEnumType{
+public enum Position implements DocsEnumType {
     FRONT("프론트엔드"),
     BACK("벡엔드"),
-    MOBILE("안드로이드"),
+    MOBILE("안드로이드/IOS"),
     DEVOPS("데브옵스");
 
     private final String description;
