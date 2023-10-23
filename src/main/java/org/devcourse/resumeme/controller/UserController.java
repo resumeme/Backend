b35bbbb9012314329f6e424dbd934bd25fc2fb05
ddongpuri <fnffnfkffk1112@gmail.com>
2023-10-23 15:51:38 +0900

    fix : 소셜로그인 저장 로직 핸들러 매핑 오류 수정

--- src/main/java/org/devcourse/resumeme/controller/UserController.java
+++ src/main/java/org/devcourse/resumeme/controller/UserController.java
@@ -8,9 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/users")
+@RestController
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
