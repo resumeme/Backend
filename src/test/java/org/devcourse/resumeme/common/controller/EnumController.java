e2b4f58c0bca415ce2789dae7cac9797dff5b910
JuHongseok <wnghdtjr129@naver.com>
2023-10-23 23:03:57 +0900

    test: enum code 문서화 처리

--- src/test/java/org/devcourse/resumeme/common/controller/EnumController.java
+++ src/test/java/org/devcourse/resumeme/common/controller/EnumController.java
@@ -1,6 +1,8 @@
 package org.devcourse.resumeme.common.controller;
 
 import org.devcourse.resumeme.common.domain.DocsEnumType;
+import org.devcourse.resumeme.common.domain.Position;
+import org.devcourse.resumeme.domain.event.EventStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +15,10 @@ public class EnumController {
 
     @GetMapping("/enums")
     public EnumDocsResponse enums() {
-        return new EnumDocsResponse(to(null));
+        return new EnumDocsResponse(
+                to(Position.values()),
+                to(EventStatus.values())
+        );
     }
 
     private Map<String, String> to(DocsEnumType[] enumTypes) {
@@ -21,7 +26,7 @@ public class EnumController {
                 .collect(Collectors.toMap(DocsEnumType::getType, DocsEnumType::getDescription));
     }
 
-    public record EnumDocsResponse(Map<String, String> enumCode) {
+    public record EnumDocsResponse(Map<String, String> position, Map<String, String> eventStatus) {
 
     }
 
