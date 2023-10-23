cb10eee80f68d0c75d0cc3221518fbdc79d67090
ddongpuri <fnffnfkffk1112@gmail.com>
2023-10-23 16:05:03 +0900

    fix : 불필요한 역할 제거 및 CustomException 추가

--- src/main/java/org/devcourse/resumeme/domain/user/Role.java
+++ src/main/java/org/devcourse/resumeme/domain/user/Role.java
@@ -1,16 +1,17 @@
 package org.devcourse.resumeme.domain.user;
 
+import org.devcourse.resumeme.global.advice.exception.CustomException;
+
 public enum Role {
     ROLE_GUEST, // 소셜로그인 만 마친 상태 (추가 필수 정보 기입 x)
     ROLE_MENTEE,// 소셜로그인 후 추가 필수 정보 기입 (멘티)
-    ROLE_TEMP, // 소셜로그인 후 추가 필수 정보 기입 (멘토) + 아직 멘토 승인 요청 안보낸 상태
-    ROLE_PENDING, // 소셜로그인 후 추가 필수 정보 기입 (멘토) + 멘토 승인 요청 보내고 심사 대기중
+    ROLE_PENDING, // 소셜로그인 후 추가 필수 정보 기입 (멘토)
     ROLE_MENTOR, // 멘토 승인 완료
     ROLE_ADMIN;
 
     public void checkSignUpRequest() {
-        if (this != Role.ROLE_TEMP && this != Role.ROLE_MENTEE) {
-            throw new IllegalArgumentException("잘못된 요청입니다.");
+        if (this != Role.ROLE_PENDING && this != Role.ROLE_MENTEE) {
+            throw new CustomException("ROLE_CHANGE_REFUSED","해당 역할로 변경할 수 없습니다.");
         }
     }
 }
