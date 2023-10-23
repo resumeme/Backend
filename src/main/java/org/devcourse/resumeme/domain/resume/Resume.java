391c860b6b0ad024292ffb175d16bec0202615f4
bumwoo <gld55602@gmail.com>
2023-10-23 16:15:47 +0900

    feat: 생성자 추가 및 작성중, 대표이력서 지정 메소드 추가

--- src/main/java/org/devcourse/resumeme/domain/resume/Resume.java
+++ src/main/java/org/devcourse/resumeme/domain/resume/Resume.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.devcourse.resumeme.common.domain.Position;
@@ -18,7 +17,6 @@ import static org.devcourse.resumeme.common.util.Validator.validate;
 
 @Entity
 @NoArgsConstructor
-@AllArgsConstructor
 public class Resume {
 
     @Id
@@ -68,6 +66,36 @@ public class Resume {
     private void validateInput(String title, String introduce) {
         validate(title == null, ExceptionCode.NO_EMPTY_VALUE);
         validate(introduce == null, ExceptionCode.NO_EMPTY_VALUE);
+    }
+
+    public Resume(String title, boolean isWriting, boolean isRepresentative, Long userId, Position position,
+                  String introduce, Training training, Career career, Project project, String email,
+                  String githubAddress, String blogAddress, String phoneNumber) {
+        this.title = title;
+        this.isWriting = isWriting;
+        this.isRepresentative = isRepresentative;
+        this.userId = userId;
+        this.position = position;
+        this.introduce = introduce;
+        this.training = training;
+        this.career = career;
+        this.project = project;
+        this.email = email;
+        this.githubAddress = githubAddress;
+        this.blogAddress = blogAddress;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void writing() {
+        this.isWriting = true;
+    }
+
+    public void fininsh() {
+        this.isWriting = false;
+    }
+
+    public void decideRepresentative() {
+        this.isRepresentative = true;
     }
 
 }
