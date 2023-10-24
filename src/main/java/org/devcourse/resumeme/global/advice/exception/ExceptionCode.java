2f6654c85ac105419ab61e3d78d4d704e6fb68a9
bumwoo <gld55602@gmail.com>
2023-10-24 16:49:55 +0900

    feat: MENTEE_ONLY_RESUME 추가

--- src/main/java/org/devcourse/resumeme/global/advice/exception/ExceptionCode.java
+++ src/main/java/org/devcourse/resumeme/global/advice/exception/ExceptionCode.java
@@ -2,7 +2,8 @@ package org.devcourse.resumeme.global.advice.exception;
 
 public enum ExceptionCode {
     MENTEE_NOT_FOUND("해당 멘티를 찾을 수 없습니다."),
-    NO_EMPTY_VALUE("빈 값일 수 없습니다");
+    NO_EMPTY_VALUE("빈 값일 수 없습니다"),
+    MENTEE_ONLY_RESUME("멘티만 이력서를 작성할 수 있습니다.");
 
     private final String message;
 
