ba531cab64ed4022ce826e194b76e1448c2da0c7
ddongpuri <fnffnfkffk1112@gmail.com>
2023-10-25 14:09:31 +0900

    fix: 리뷰 사항 반영

--- src/main/java/org/devcourse/resumeme/service/MenteeService.java
+++ src/main/java/org/devcourse/resumeme/service/MenteeService.java
@@ -1,11 +1,11 @@
 package org.devcourse.resumeme.service;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.devcourse.resumeme.domain.mentee.Mentee;
 import org.devcourse.resumeme.global.advice.exception.CustomException;
 import org.devcourse.resumeme.repository.MenteeRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +13,7 @@ public class MenteeService {
 
     private final MenteeRepository menteeRepository;
 
+    @Transactional(readOnly = true)
     public Mentee getOne(Long id) {
         return menteeRepository.findById(id).orElseThrow(() -> new CustomException("MENTEE_NOT_FOUND", "존재하지 않는 회원입니다."));
     }
--- src/main/java/org/devcourse/resumeme/service/MentorService.java
+++ src/main/java/org/devcourse/resumeme/service/MentorService.java
@@ -1,11 +1,11 @@
 package org.devcourse.resumeme.service;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.devcourse.resumeme.domain.mentor.Mentor;
 import org.devcourse.resumeme.global.advice.exception.CustomException;
 import org.devcourse.resumeme.repository.MentorRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +13,7 @@ public class MentorService {
 
     private final MentorRepository mentorRepository;
 
+    @Transactional(readOnly = true)
     public Mentor getOne(Long id) {
         return mentorRepository.findById(id).orElseThrow(() -> new CustomException("MENTOR_NOT_FOUND", "존재하지 않는 회원입니다."));
     }
