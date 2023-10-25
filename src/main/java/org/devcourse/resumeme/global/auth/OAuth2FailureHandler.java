320ab6e23baa7a0a2bc854051110211d201c5876
ddongpuri <fnffnfkffk1112@gmail.com>
2023-10-26 02:23:44 +0900

    feat: OAuth2Handler 구현

---
+++ src/main/java/org/devcourse/resumeme/global/auth/OAuth2FailureHandler.java
@@ -0,0 +1,30 @@
+package org.devcourse.resumeme.global.auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2FailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        String cacheKey = exception.getMessage();
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.addHeader("cacheKey", cacheKey);
+        response.addHeader("location", "/api/v1/user-info-form");
+    }
+
+}
