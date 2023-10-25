4f90221fbb666c20fa3cabe39950b3a1c69c7cd0
bumwoo <gld55602@gmail.com>
2023-10-25 17:31:50 +0900

    feat: 프로젝트 등록 기능 구현

--- src/main/java/org/devcourse/resumeme/domain/resume/Project.java
+++ src/main/java/org/devcourse/resumeme/domain/resume/Project.java
@@ -1,24 +1,66 @@
 package org.devcourse.resumeme.domain.resume;
 
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.devcourse.resumeme.global.advice.exception.ExceptionCode;
 
 import java.time.LocalDate;
+import java.util.List;
 
-@Embeddable
-@NoArgsConstructor
+import static org.devcourse.resumeme.common.util.Validator.validate;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Project {
+
+    @Id
+    @Getter
+    @GeneratedValue
+    @Column(name = "project_id")
+    private Long id;
 
     private String projectName;
 
-    private LocalDate projectStartDate;
+    private Long productionYear;
+
+    private boolean isTeam;
+
+    private String TeamMembers;
+
+    @ElementCollection
+    @CollectionTable(name = "career_skills")
+    private List<Skill> skills;
 
     private String projectContent;
 
-    public Project(String projectName, LocalDate projectStartDate, String projectContent) {
+    private String projectUrl;
+
+    public Project(String projectName, Long productionYear, boolean isTeam, String teamMembers, List<Skill> skills,
+                   String projectContent, String projectUrl) {
+        validateProject(projectName, productionYear);
+
         this.projectName = projectName;
-        this.projectStartDate = projectStartDate;
+        this.productionYear = productionYear;
+        this.isTeam = isTeam;
+        if (isTeam) {
+            this.TeamMembers = teamMembers;
+        }
+        this.skills = skills;
         this.projectContent = projectContent;
+        this.projectUrl = projectUrl;
+    }
+
+    private void validateProject(String projectName, Long productionYear) {
+        validate(projectName == null, ExceptionCode.NO_EMPTY_VALUE);
+        validate(productionYear == null, ExceptionCode.NO_EMPTY_VALUE);
     }
 
 }
