287c2a8875de5dc6e99dd04c76134190e3b370f7
JuHongseok <wnghdtjr129@naver.com>
2023-10-25 23:50:13 +0900

    test: 반려기능 api, service테스트 작성

--- src/test/java/org/devcourse/resumeme/service/EventServiceTest.java
+++ src/test/java/org/devcourse/resumeme/service/EventServiceTest.java
@@ -3,9 +3,11 @@ package org.devcourse.resumeme.service;
 import org.devcourse.resumeme.domain.event.Event;
 import org.devcourse.resumeme.domain.event.EventInfo;
 import org.devcourse.resumeme.domain.event.EventTimeInfo;
+import org.devcourse.resumeme.domain.event.exception.EventException;
 import org.devcourse.resumeme.domain.mentor.Mentor;
 import org.devcourse.resumeme.repository.EventRepository;
 import org.devcourse.resumeme.service.vo.AcceptMenteeToEvent;
+import org.devcourse.resumeme.service.vo.EventReject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -16,7 +18,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
@@ -27,7 +28,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -93,6 +97,39 @@ class EventServiceTest {
                 }
                 countDownLatch.countDown();
             });
+    }
+
+    @Test
+    void 이벤트_신청을_반려에_성공한다() {
+        // given
+        EventInfo openEvent = EventInfo.open(3, "제목", "내용");
+        EventTimeInfo eventTimeInfo = EventTimeInfo.onStart(LocalDateTime.now(), LocalDateTime.now().plusHours(1L), LocalDateTime.now().plusHours(2L));
+        Event event = new Event(openEvent, eventTimeInfo, new Mentor(), List.of());
+        event.acceptMentee(1L, 1L);
+
+        EventReject reject = new EventReject(1L, 1L, "이력서 양이 너무 적습니다");
+        lenient().when(eventRepository.findById(1L)).thenReturn(Optional.of(event));
+
+        // when
+        eventService.reject(reject);
+
+        // then
+        verify(eventRepository).findById(1L);
+    }
+
+    @Test
+    void 이벤트_신청을_반려에_실패한다_신청한_멘티가_없음() {
+        // given
+        EventInfo openEvent = EventInfo.open(3, "제목", "내용");
+        EventTimeInfo eventTimeInfo = EventTimeInfo.onStart(LocalDateTime.now(), LocalDateTime.now().plusHours(1L), LocalDateTime.now().plusHours(2L));
+        Event event = new Event(openEvent, eventTimeInfo, new Mentor(), List.of());
+        event.acceptMentee(1L, 1L);
+
+        EventReject reject = new EventReject(1L, 2L, "이력서 양이 너무 적습니다");
+        given(eventRepository.findById(1L)).willReturn(Optional.of(event));
+
+        // when & then
+        assertThatThrownBy(() -> eventService.reject(reject)).isInstanceOf(EventException.class);
     }
 
 }
