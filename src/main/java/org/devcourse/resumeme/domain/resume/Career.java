9d5935384dce88cb3ee1a0c426fcbda867cd209e
bumwoo <gld55602@gmail.com>
2023-10-25 15:00:44 +0900

    feat: 리뷰 내용 반영

--- src/main/java/org/devcourse/resumeme/domain/resume/Career.java
+++ src/main/java/org/devcourse/resumeme/domain/resume/Career.java
@@ -1,13 +1,17 @@
 package org.devcourse.resumeme.domain.resume;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.devcourse.resumeme.common.domain.Position;
 import org.devcourse.resumeme.global.advice.exception.CustomException;
 import org.devcourse.resumeme.global.advice.exception.ExceptionCode;
 
@@ -28,11 +32,18 @@ public class Career {
 
     private String companyName;
 
+    @Enumerated(EnumType.STRING)
+    private Position position;
+
     @ElementCollection
+    @CollectionTable(name = "career_skills")
     private List<Skill> skills;
 
     @ElementCollection
+    @CollectionTable(name = "career_duties")
     private List<Duty> duties;
+
+    private boolean isCurrentlyEmployed;
 
     private LocalDate careerStartDate;
 
@@ -41,23 +52,29 @@ public class Career {
     private String careerContent;
 
 
-    public Career(String companyName, List<Skill> skills, List<Duty> duties, LocalDate careerStartDate, LocalDate endDate, String careerContent) {
-        validateCareer(companyName, skills, duties, careerStartDate, endDate);
+    public Career(String companyName, Position position, List<Skill> skills, List<Duty> duties, boolean isCurrentlyEmployed,
+                  LocalDate careerStartDate, LocalDate endDate, String careerContent) {
+        validateCareer(companyName, position, skills, duties, isCurrentlyEmployed, careerStartDate, endDate);
 
         this.companyName = companyName;
+        this.position = position;
         this.skills = skills;
         this.duties = duties;
+        this.isCurrentlyEmployed = isCurrentlyEmployed;
         this.careerStartDate = careerStartDate;
         this.endDate = endDate;
         this.careerContent = careerContent;
     }
 
-    private void validateCareer(String companyName, List<Skill> skills, List<Duty> duties, LocalDate careerStartDate, LocalDate endDate) {
+    private void validateCareer(String companyName, Position position, List<Skill> skills, List<Duty> duties, boolean isCurrentlyEmployed, LocalDate careerStartDate, LocalDate endDate) {
         validate(companyName == null, ExceptionCode.NO_EMPTY_VALUE);
+        validate(position == null, ExceptionCode.NO_EMPTY_VALUE);
         validate(skills.isEmpty(), ExceptionCode.NO_EMPTY_VALUE);
         validate(duties.isEmpty(), ExceptionCode.NO_EMPTY_VALUE);
         validate(careerStartDate == null, ExceptionCode.NO_EMPTY_VALUE);
-        validate(endDate == null, ExceptionCode.NO_EMPTY_VALUE);
+        if (isCurrentlyEmployed) {
+            validate(endDate == null, ExceptionCode.NO_EMPTY_VALUE);
+        }
 
         if (careerStartDate.isAfter(endDate)) {
             throw new CustomException("TIME_ERROR", "시작일은 종료일보다 먼저여야 합니다.");
--- src/test/java/org/devcourse/resumeme/domain/resume/CareerTest.java
+++ src/test/java/org/devcourse/resumeme/domain/resume/CareerTest.java
@@ -1,6 +1,8 @@
 package org.devcourse.resumeme.domain.resume;
 
+import org.devcourse.resumeme.common.domain.Position;
 import org.devcourse.resumeme.global.advice.exception.CustomException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -14,29 +16,48 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class CareerTest {
 
+    private String companyName;
+    private Position position;
+    private List<Skill> skills;
+    private List<Duty> duties;
+    private boolean isCurrentlyEmployed;
+    private String careerContent;
+
+    @BeforeEach
+    void init() {
+        companyName = "Company";
+        position = Position.BACK;
+        skills = new ArrayList<>();
+        duties = new ArrayList<>();
+        isCurrentlyEmployed = true;
+        careerContent = "상세 업무";
+    }
+
     @Test
     void 기술_스택_및_업무_내용_없을_시_예외_발생() {
-        String companyName = "Company";
-        List<Skill> skills = new ArrayList<>();
-        List<Duty> duties = new ArrayList<>();
         LocalDate careerStartDate = LocalDate.now();
         LocalDate endDate = LocalDate.now().plusYears(2);
-        String careerContent = "상세 업무";
 
-        assertThatThrownBy(() -> new Career(companyName, skills, duties, careerStartDate, endDate, careerContent))
+        assertThatThrownBy(() -> new Career(companyName, position, skills, duties, isCurrentlyEmployed, careerStartDate, endDate, careerContent))
                 .isInstanceOf(CustomException.class);
     }
 
     @Test
     void 종료일이_시작일_보다_먼저일_시_예외_발생() {
-        String companyName = "Company";
-        List<Skill> skills = new ArrayList<>();
-        List<Duty> duties = new ArrayList<>();
         LocalDate careerStartDate = LocalDate.now();
         LocalDate endDate = LocalDate.now().minusYears(1);
+
+        assertThatThrownBy(() -> new Career(companyName, position, skills, duties, isCurrentlyEmployed, careerStartDate, endDate, careerContent))
+                .isInstanceOf(CustomException.class);
+    }
+
+    @Test
+    void 재직중일시_종료일_필수_입력() {
+        LocalDate careerStartDate = LocalDate.now();
+        LocalDate endDate = null;
         String careerContent = "상세 업무";
 
-        assertThatThrownBy(() -> new Career(companyName, skills, duties, careerStartDate, endDate, careerContent))
+        assertThatThrownBy(() -> new Career(companyName, position, skills, duties, isCurrentlyEmployed, careerStartDate, endDate, careerContent))
                 .isInstanceOf(CustomException.class);
     }
 
