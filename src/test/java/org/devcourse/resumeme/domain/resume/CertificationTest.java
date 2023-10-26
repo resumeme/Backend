beb6dfc22d6fa6295bea253fc7afbdbf81ddd570
bumwoo <gld55602@gmail.com>
2023-10-26 14:44:21 +0900

    test: 엔티티 생성 성공 테스트 추가

--- src/test/java/org/devcourse/resumeme/domain/resume/CertificationTest.java
+++ src/test/java/org/devcourse/resumeme/domain/resume/CertificationTest.java
@@ -1,15 +1,25 @@
 package org.devcourse.resumeme.domain.resume;
 
 import org.devcourse.resumeme.global.advice.exception.CustomException;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class CertificationTest {
 
     @Test
+    public void 수상_및_자격증_생성에_성공한다() {
+        Certification certification = new Certification("제목", "2023-01-01", "수여 기관", "Link", "Description");
+        assertThat(certification).isNotNull();
+    }
+
+    @Test
     public void 제목이_비어있을_때_예외_발생() {
-        assertThatThrownBy(() -> new Certification(null, "2023-01-01", "Issuing Authority", "Link", "Description"))
+        assertThatThrownBy(() -> new Certification(null, "2023-01-01", "수여 기관", "Link", "Description"))
                 .isInstanceOf(CustomException.class);
     }
 
