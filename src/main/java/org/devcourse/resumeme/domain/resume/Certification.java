30a06ecb167c36c29472d59f604098e78358a3af
beomukim <86512449+beomukim@users.noreply.github.com>
2023-10-26 16:16:20 +0900

    수상 및 자격 등록 기능 추가 (#48)

---
+++ src/main/java/org/devcourse/resumeme/domain/resume/Certification.java
@@ -0,0 +1,44 @@
+package org.devcourse.resumeme.domain.resume;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.devcourse.resumeme.global.advice.exception.ExceptionCode;
+
+import static org.devcourse.resumeme.common.util.Validator.Condition.isBlank;
+import static org.devcourse.resumeme.common.util.Validator.validate;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Certification {
+
+    @Id
+    @Getter
+    @GeneratedValue
+    @Column(name = "certification_id")
+    private Long id;
+
+    private String certificationTitle;
+
+    private String acquisitionDate;
+
+    private String issuingAuthority;
+
+    private String link;
+
+    private String description;
+
+    public Certification(String certificationTitle, String acquisitionDate, String issuingAuthority, String link, String description) {
+        validate(isBlank(certificationTitle), ExceptionCode.NO_EMPTY_VALUE);
+        this.certificationTitle = certificationTitle;
+        this.acquisitionDate = acquisitionDate;
+        this.issuingAuthority = issuingAuthority;
+        this.link = link;
+        this.description = description;
+    }
+
+}
