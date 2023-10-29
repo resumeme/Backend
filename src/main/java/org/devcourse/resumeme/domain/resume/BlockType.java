5c7b3abbf764ae1850a70494cabef15e084d6746
HongSeok Ju <75611167+Juhongseok@users.noreply.github.com>
2023-10-29 09:40:35 +0900

    이벤트 리뷰 도메인 도출 (#57)

---
+++ src/main/java/org/devcourse/resumeme/domain/resume/BlockType.java
@@ -0,0 +1,14 @@
+package org.devcourse.resumeme.domain.resume;
+
+public enum BlockType {
+
+    ACTIVITY,
+    CAREER,
+    CERTIFICATION,
+    DUTY,
+    FOREIGN_LANGUAGE,
+    PROJECT,
+    SKILL,
+    TRAINING;
+
+}
