e34307eef899af5b6a65af1bfa6eb30b5f97282d
JuHongseok <wnghdtjr129@naver.com>
2023-10-30 14:58:29 +0900

    feat: 멘토 가입 신청 리스트 도메인 구현

---
+++ src/main/java/org/devcourse/resumeme/domain/admin/MentorApplication.java
@@ -0,0 +1,30 @@
+package org.devcourse.resumeme.domain.admin;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+import static org.devcourse.resumeme.common.util.Validator.validate;
+import static org.devcourse.resumeme.global.advice.exception.ExceptionCode.NO_EMPTY_VALUE;
+
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+public class MentorApplication {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "mentor_application_id")
+    private Long id;
+
+    private Long mentorId;
+
+    public MentorApplication(Long mentorId) {
+        validate(mentorId == null, NO_EMPTY_VALUE);
+
+        this.mentorId = mentorId;
+    }
+
+}
