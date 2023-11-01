8e15040741863895963346a59f22c58d4965cb91
JuHongseok <wnghdtjr129@naver.com>
2023-11-02 00:08:40 +0900

    feat: controller advice 추가

---
+++ src/main/java/org/devcourse/resumeme/global/advice/ExceptionControllerAdvice.java
@@ -0,0 +1,27 @@
+package org.devcourse.resumeme.global.advice;
+
+import org.devcourse.resumeme.global.advice.exception.CustomException;
+import org.devcourse.resumeme.global.advice.exception.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.METHOD_NOT_ALLOWED;
+
+public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(CustomException.class)
+    public ErrorResponse customExceptionHandle(CustomException exception) {
+        return new ErrorResponse(exception.getMessage(), exception.getCode());
+    }
+
+    @ResponseStatus(METHOD_NOT_ALLOWED)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorResponse methodArgumentNotValidHandle(MethodArgumentNotValidException exception) {
+        return new ErrorResponse("endpoint, method를 다시 확인해주세요", "NOT_MATCH_METHOD_ENDPOINT");
+    }
+
+}
