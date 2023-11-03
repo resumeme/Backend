e8210cdbd7a6035f1ff2c82f7e3c0af3244780c5
ddongpuri <fnffnfkffk1112@gmail.com>
2023-11-03 18:03:58 +0900

    fix: RestControllerAdvice 어노테이션 추가

--- src/main/java/org/devcourse/resumeme/global/advice/ExceptionControllerAdvice.java
+++ src/main/java/org/devcourse/resumeme/global/advice/ExceptionControllerAdvice.java
@@ -3,15 +3,15 @@ package org.devcourse.resumeme.global.advice;
 import org.devcourse.resumeme.global.advice.exception.CustomException;
 import org.devcourse.resumeme.global.advice.exception.ErrorResponse;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.METHOD_NOT_ALLOWED;
 
-public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
 
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(CustomException.class)
