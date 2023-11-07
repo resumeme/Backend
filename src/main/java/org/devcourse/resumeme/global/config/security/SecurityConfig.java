76767c9cb02234c4c8537a756ea257163c1a0186
JuHongseok <wnghdtjr129@naver.com>
2023-11-07 16:58:45 +0900

    feat: 같은 엔드포인트에 대해 여러 role 적용 구현

--- src/main/java/org/devcourse/resumeme/global/config/security/SecurityConfig.java
+++ src/main/java/org/devcourse/resumeme/global/config/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.devcourse.resumeme.global.auth.filter.ExceptionHandlerFilter;
 import org.devcourse.resumeme.global.auth.filter.JwtAuthorizationFilter;
 import org.devcourse.resumeme.global.auth.filter.OAuthTokenResponseFilter;
 import org.devcourse.resumeme.global.config.security.properties.EndpointProperties;
+import org.devcourse.resumeme.global.config.security.properties.EndpointProperties.Matcher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -13,10 +14,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.util.List;
-import java.util.Map;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -54,17 +51,9 @@ public class SecurityConfig {
     }
 
     private void setEndpoints(HttpSecurity http) throws Exception {
-        for (EndpointProperties.Matcher matcher : properties.matchers()) {
-            for (Map.Entry<String, List<String>> entry : matcher.matcher().entrySet()) {
-                String method = entry.getKey().toUpperCase();
-
-                for (String endPoint : entry.getValue()) {
-                    http.authorizeHttpRequests(registry ->
-                            registry.requestMatchers(new AntPathRequestMatcher(endPoint, method))
-                                    .access(matcher.manager())
-                    );
-                }
-            }
+        for (Matcher matcher : properties.matchers()) {
+            http.authorizeHttpRequests(registry ->
+                    registry.requestMatchers(matcher.requestMatcher()).access(matcher.manager()));
         }
 
     }
--- src/main/java/org/devcourse/resumeme/global/config/security/properties/EndpointProperties.java
+++ src/main/java/org/devcourse/resumeme/global/config/security/properties/EndpointProperties.java
@@ -1,78 +1,155 @@
 package org.devcourse.resumeme.global.config.security.properties;
 
 import jakarta.annotation.PostConstruct;
-import org.devcourse.resumeme.global.auth.service.authorization.AuthorizationResolver;
-import org.devcourse.resumeme.global.auth.service.authorization.OnlyOwn;
 import org.devcourse.resumeme.business.event.repository.EventRepository;
 import org.devcourse.resumeme.business.resume.repository.ResumeRepository;
+import org.devcourse.resumeme.global.auth.service.authorization.AuthorizationResolver;
+import org.devcourse.resumeme.global.auth.service.authorization.OnlyOwn;
+import org.devcourse.resumeme.global.config.security.properties.EndpointProperties.Matcher.Request;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.security.authorization.AuthorityAuthorizationManager;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @ConfigurationProperties("endpoint")
-public record EndpointProperties(List<Matcher> matchers, List<String> ignores) {
+public class EndpointProperties {
 
-    public record Matcher(Map<String, List<String>> matcher, String role) {
+    private final List<Matcher> matchers;
 
-        public AuthorizationManager<RequestAuthorizationContext> manager() {
-            return Manager.getManager(role);
+    private final List<String> ignores;
+
+    public EndpointProperties(List<PropertyMatcher> matchers, List<String> ignores) {
+        this.matchers = getMatchers(matchers);
+        this.ignores = ignores;
+    }
+
+    private static List<Matcher> getMatchers(List<PropertyMatcher> matchers) {
+        Map<Request, List<String>> tempMatcher = new HashMap<>();
+
+        for (PropertyMatcher matcher : matchers) {
+            String role = matcher.role.toUpperCase();
+
+            for (Map.Entry<String, List<String>> entry : matcher.matcher.entrySet()) {
+                String method = entry.getKey().toUpperCase();
+
+                for (String endpoint : entry.getValue()) {
+                    Request endpoint1 = new Request(method, endpoint);
+                    List<String> roles = tempMatcher.getOrDefault(endpoint1, new ArrayList<>());
+                    roles.add(role);
+                    tempMatcher.put(endpoint1, roles);
+                }
+            }
         }
+
+        return tempMatcher.entrySet().stream()
+                .map(entry -> new Matcher(entry.getKey(), entry.getValue()))
+                .toList();
+    }
+
+    public List<Matcher> matchers() {
+        return matchers;
+    }
+
+    public List<String> ignores() {
+        return ignores;
+    }
+
+    public record PropertyMatcher(Map<String, List<String>> matcher, String role) {
 
     }
 
-    enum Manager {
+    public record Matcher(Request request, List<String> roles) {
 
-        ALL((a, o) -> new AuthorizationDecision(true)),
-        OWN_MENTEE(),
-        OWN_MENTOR();
+        record Request(String method, String endpoint) {
 
-        AuthorizationManager<RequestAuthorizationContext> manager;
-
-        Manager() {
-        }
-
-        Manager(AuthorizationManager<RequestAuthorizationContext> manager) {
-            this.manager = manager;
-        }
-
-        static AuthorizationManager<RequestAuthorizationContext> getManager(String role) {
-            String roleName = role.toUpperCase();
-
-            for (Manager value : values()) {
-                if (value.name().equals(roleName)) {
-                    return value.manager;
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) {
+                    return true;
                 }
+                if (o == null || getClass() != o.getClass()) {
+                    return false;
+                }
+                Request endpoint1 = (Request) o;
+                return Objects.equals(method, endpoint1.method) && Objects.equals(endpoint, endpoint1.endpoint);
             }
 
-            return AuthorityAuthorizationManager.hasRole(roleName);
-        }
-
-        @Component
-        public static class ManagerInjector {
-
-            private final ResumeRepository resumeRepository;
-
-            private final EventRepository eventRepository;
-
-            public ManagerInjector(EventRepository eventRepository, ResumeRepository resumeRepository) {
-                this.eventRepository = eventRepository;
-                this.resumeRepository = resumeRepository;
-            }
-
-            @PostConstruct
-            public void postConstruct() {
-                AuthorizationResolver resolver = new AuthorizationResolver(resumeRepository, eventRepository);
-                OWN_MENTEE.manager = new OnlyOwn("resumes", "ROLE_MENTEE", resolver);
-                OWN_MENTOR.manager = new OnlyOwn("events", "ROLE_MENTOR", resolver);
+            @Override
+            public int hashCode() {
+                return Objects.hash(method, endpoint);
             }
 
         }
+
+        public RequestMatcher requestMatcher() {
+            return new AntPathRequestMatcher(request.endpoint, request.method);
+        }
+
+        public AuthorizationManager<RequestAuthorizationContext> manager() {
+            return Manager.getManager(roles);
+        }
+
+        enum Manager {
+
+            ALL((a, o) -> new AuthorizationDecision(true)),
+            OWN_MENTEE(),
+            OWN_MENTOR();
+
+            AuthorizationManager<RequestAuthorizationContext> authorizationManager;
+
+            Manager() {
+            }
+
+            Manager(AuthorizationManager<RequestAuthorizationContext> authorizationManager) {
+                this.authorizationManager = authorizationManager;
+            }
+
+            static AuthorizationManager<RequestAuthorizationContext> getManager(List<String> role) {
+                if (role.size() == 1) {
+                    String roleName = role.get(0).toUpperCase();
+
+                    for (Manager value : values()) {
+                        if (value.name().equals(roleName)) {
+                            return value.authorizationManager;
+                        }
+                    }
+                }
+
+                return AuthorityAuthorizationManager.hasAnyRole(role.toArray(new String[]{}));
+            }
+
+            @Component
+            public static class ManagerInjector {
+
+                private final ResumeRepository resumeRepository;
+
+                private final EventRepository eventRepository;
+
+                public ManagerInjector(EventRepository eventRepository, ResumeRepository resumeRepository) {
+                    this.eventRepository = eventRepository;
+                    this.resumeRepository = resumeRepository;
+                }
+
+                @PostConstruct
+                public void postConstruct() {
+                    AuthorizationResolver resolver = new AuthorizationResolver(resumeRepository, eventRepository);
+                    OWN_MENTEE.authorizationManager = new OnlyOwn("resumes", "ROLE_MENTEE", resolver);
+                    OWN_MENTOR.authorizationManager = new OnlyOwn("events", "ROLE_MENTOR", resolver);
+                }
+
+            }
+        }
+
     }
 
 }
