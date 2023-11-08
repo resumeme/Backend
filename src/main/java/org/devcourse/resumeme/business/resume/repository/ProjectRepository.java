98222e6c0bdd9fb31a11dfc7e702e1da2a8de5c9
JuHongseok <wnghdtjr129@naver.com>
2023-11-08 22:42:59 +0900

    feat: 미사용 삭제

--- src/main/java/org/devcourse/resumeme/business/resume/repository/ProjectRepository.java
+++
@@ -1,8 +0,0 @@
-package org.devcourse.resumeme.business.resume.repository;
-
-import org.devcourse.resumeme.business.resume.domain.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-
-}
--- src/main/java/org/devcourse/resumeme/business/resume/repository/TrainingRepository.java
+++
@@ -1,8 +0,0 @@
-package org.devcourse.resumeme.business.resume.repository;
-
-import org.devcourse.resumeme.business.resume.domain.Training;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrainingRepository extends JpaRepository<Training, Long> {
-
-}
