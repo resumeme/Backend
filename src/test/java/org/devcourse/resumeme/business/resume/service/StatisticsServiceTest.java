2831f87ce8abd3854158d2481607a0126426773c
bumwoo <gld55602@gmail.com>
2023-11-11 01:35:35 +0900

    test: DisplayNameGeneration 적용, 패키지 이동

--- src/test/java/org/devcourse/resumeme/business/resume/service/StatisticsServiceTest.java
+++ src/test/java/org/devcourse/resumeme/business/resume/service/StatisticsServiceTest.java
@@ -1,17 +1,20 @@
-package org.devcourse.resumeme.business.user.service.statistics;
+package org.devcourse.resumeme.business.resume.service;
 
 import org.devcourse.resumeme.business.resume.repository.ResumeRepository;
 import org.devcourse.resumeme.business.statistics.service.StatisticsService;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class StatisticsServiceTest {
 
     @Test
-    public void testCalculatePassRate() {
+    public void 합격률_계산에_성공한다() {
         ResumeRepository resumeRepository = Mockito.mock(ResumeRepository.class);
 
         when(resumeRepository.count()).thenReturn(10L);
