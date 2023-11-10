5818fb63ca6cc728bbcf293aec1b7376aa9cc1d7
ddongpuri <fnffnfkffk1112@gmail.com>
2023-11-10 18:46:46 +0900

    test: 변경사항 테스트 코드 적용

--- src/test/java/org/devcourse/resumeme/common/util/DocumentLinkGenerator.java
+++ src/test/java/org/devcourse/resumeme/common/util/DocumentLinkGenerator.java
@@ -12,7 +12,8 @@ public interface DocumentLinkGenerator {
         PROGRESS("progress", "이벤트 참여 상태"),
         BLOCK_TYPE("blockType", "블럭 타입"),
         PROVIDER("provider", "oauth 로그인 타입"),
-        ROLE("role", "사용자 역할")
+        ROLE("role", "사용자 역할"),
+        FIELD("field", "산업 도메인")
         ;
 
         private final String htmlFileName;
