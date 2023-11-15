0ce2694ff21678f04a7e6172ef242f623a7f7f2b
JuHongseok <wnghdtjr129@naver.com>
2023-11-15 11:36:18 +0900

    feat: 이력서 기본 정보 조회 기능 추가

---
+++ src/main/java/org/devcourse/resumeme/business/resume/controllerl/dto/BasicResumeInfo.java
@@ -0,0 +1,24 @@
+package org.devcourse.resumeme.business.resume.controllerl.dto;
+
+import org.devcourse.resumeme.business.resume.domain.Resume;
+import org.devcourse.resumeme.business.user.domain.mentee.Mentee;
+
+import java.util.List;
+
+public record BasicResumeInfo(String title, String position, List<String> skills, String introduce,
+                              OwnerInfo ownerInfo) {
+
+    public BasicResumeInfo(Resume resume) {
+        this(resume.getTitle(), resume.getResumeInfo().getPosition(), resume.getResumeInfo().getSkills(), resume.getResumeInfo().getIntroduce(),
+                new OwnerInfo(resume.getMentee()));
+    }
+
+    record OwnerInfo(Long id, String name, String phoneNumber) {
+
+        public OwnerInfo(Mentee mentee) {
+            this(mentee.getId(), mentee.getRequiredInfo().getRealName(), mentee.getRequiredInfo().getPhoneNumber());
+        }
+
+    }
+
+}
--- src/main/java/org/devcourse/resumeme/business/resume/repository/ResumeRepository.java
+++ src/main/java/org/devcourse/resumeme/business/resume/repository/ResumeRepository.java
@@ -1,15 +1,20 @@
 package org.devcourse.resumeme.business.resume.repository;
 
 import org.devcourse.resumeme.business.resume.domain.Resume;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface ResumeRepository extends JpaRepository<Resume, Long> {
 
     List<Resume> findAllByMenteeId(Long menteeId);
 
     long countByCreatedDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+    @EntityGraph(attributePaths = "mentee")
+    Optional<Resume> findById(Long id);
 
 }
