051132c2a2b18f91d6868308935a58528e968d75
JuHongseok <wnghdtjr129@naver.com>
2023-11-15 23:55:18 +0900

    feat: 첨삭 수정 삭제 기능 구현

--- src/main/java/org/devcourse/resumeme/business/comment/service/CommentService.java
+++ src/main/java/org/devcourse/resumeme/business/comment/service/CommentService.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.devcourse.resumeme.business.comment.domain.Comment;
 import org.devcourse.resumeme.business.comment.repository.CommentRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class CommentService {
 
@@ -17,8 +19,27 @@ public class CommentService {
         return commentRepository.save(comment);
     }
 
+    @Transactional(readOnly = true)
+    public Comment getOne(Long commentId) {
+        return commentRepository.findById(commentId)
+                .orElseThrow();
+    }
+
+    @Transactional(readOnly = true)
     public List<Comment> getAllWithResumeId(Long resumeId) {
         return commentRepository.findAllByResumeId(resumeId);
+    }
+
+    public void update(String content, Long commentId) {
+        Comment comment = getOne(commentId);
+        comment.updateContent(content);
+    }
+
+    public void delete(Long commentId, Long resumeId) {
+        Comment comment = getOne(commentId);
+        comment.checkSameResume(resumeId);
+
+        commentRepository.delete(comment);
     }
 
 }
