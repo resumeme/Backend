21ea3fa1b71bd91bd32ea40f809ef9b4b2e0a8de
JuHongseok <75611167+Juhongseok@users.noreply.github.com>
2023-11-16 15:51:15 +0900

    type null 수정 (#186)

--- src/main/java/org/devcourse/resumeme/business/resume/controller/ComponentController.java
+++ src/main/java/org/devcourse/resumeme/business/resume/controller/ComponentController.java
@@ -7,7 +7,14 @@ import org.devcourse.resumeme.business.resume.domain.BlockType;
 import org.devcourse.resumeme.business.resume.service.ComponentService;
 import org.devcourse.resumeme.business.resume.service.ResumeService;
 import org.devcourse.resumeme.common.response.IdResponse;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -25,15 +32,14 @@ public class ComponentController {
         return new IdResponse(blockService.create(request.toEntity().of(resumeId), type));
     }
 
-    @GetMapping({"/{resumeId}","/{resumeId}/{type}"})
+    @GetMapping({"/{resumeId}", "/{resumeId}/{type}"})
     public List<ComponentResponse> getCareer(@PathVariable Long resumeId, @PathVariable(required = false) String type) {
         resumeService.getOne(resumeId);
 
         return blockService.getAll(resumeId).stream()
                 .filter(component -> component.isType(type))
-                .flatMap(component -> component.getComponents().stream())
-                .toList().stream()
-                .map(component -> BlockType.from(type, component))
+                .flatMap(component -> component.getComponents().stream()
+                        .map(subComponent -> BlockType.from(component.getProperty(), subComponent)))
                 .toList();
     }
 
