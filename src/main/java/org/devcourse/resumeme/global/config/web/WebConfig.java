045234da7bbc67d9dfbfdea3bd7a25c5a9e4c843
JuHongseok <wnghdtjr129@naver.com>
2023-11-18 16:20:55 +0900

    feat: 스케줄링을 통한 이벤트 진행 상태 값 변경

--- src/main/java/org/devcourse/resumeme/global/config/web/WebConfig.java
+++ src/main/java/org/devcourse/resumeme/global/config/web/WebConfig.java
@@ -2,8 +2,10 @@ package org.devcourse.resumeme.global.config.web;
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@EnableScheduling
 @ConfigurationPropertiesScan(basePackages = "org.devcourse.resumeme.global")
 public class WebConfig {
 
