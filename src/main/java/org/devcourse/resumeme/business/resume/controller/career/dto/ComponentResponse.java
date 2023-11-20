55ae08111445b8da7e628aa438c224bedef3c001
JuHongseok <wnghdtjr129@naver.com>
2023-11-21 01:34:50 +0900

    feat: 응답 값 변환 용 type 추가

--- src/main/java/org/devcourse/resumeme/business/resume/controller/career/dto/ComponentResponse.java
+++ src/main/java/org/devcourse/resumeme/business/resume/controller/career/dto/ComponentResponse.java
@@ -1,13 +1,38 @@
 package org.devcourse.resumeme.business.resume.controller.career.dto;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.devcourse.resumeme.business.resume.controller.dto.ActivityResponse;
+import org.devcourse.resumeme.business.resume.controller.dto.CertificationResponse;
+import org.devcourse.resumeme.business.resume.controller.dto.ForeignLanguageResponse;
+import org.devcourse.resumeme.business.resume.controller.dto.ProjectResponse;
+import org.devcourse.resumeme.business.resume.controller.dto.ResumeLinkResponse;
+import org.devcourse.resumeme.business.resume.controller.dto.TrainingResponse;
 import org.devcourse.resumeme.business.resume.entity.Component;
 
 import java.time.LocalDateTime;
 
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type"
+)
+@JsonSubTypes(value = {
+        @JsonSubTypes.Type(value = ActivityResponse.class, name = "activities"),
+        @JsonSubTypes.Type(value = CareerResponse.class, name = "careers"),
+        @JsonSubTypes.Type(value = CertificationResponse.class, name = "certifications"),
+        @JsonSubTypes.Type(value = ForeignLanguageResponse.class, name = "foreign-languages"),
+        @JsonSubTypes.Type(value = ProjectResponse.class, name = "projects"),
+        @JsonSubTypes.Type(value = TrainingResponse.class, name = "trainings"),
+        @JsonSubTypes.Type(value = ResumeLinkResponse.class, name = "links")
+})
 @NoArgsConstructor
 public abstract class ComponentResponse {
+
+    @Getter
+    protected String type;
 
     @Getter
     protected Long componentId;
@@ -16,15 +41,16 @@ public abstract class ComponentResponse {
     protected Long originComponentId;
 
     @Getter
-    protected boolean isReflectFeedback;
+    protected boolean reflectFeedback;
 
     @Getter
     private LocalDateTime createdDate;
 
-    protected ComponentResponse(Component component) {
+    protected ComponentResponse(String type, Component component) {
+        this.type = type;
         this.componentId = component.getId();
         this.originComponentId = component.getOriginComponentId();
-        this.isReflectFeedback = component.isReflectFeedBack();
+        this.reflectFeedback = component.isReflectFeedBack();
         this.createdDate = component.getCreatedDate();
     }
 
--- src/main/java/org/devcourse/resumeme/business/resume/controller/dto/ResumeLinkResponse.java
+++ src/main/java/org/devcourse/resumeme/business/resume/controller/dto/ResumeLinkResponse.java
@@ -1,18 +1,22 @@
 package org.devcourse.resumeme.business.resume.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.devcourse.resumeme.business.resume.controller.career.dto.ComponentResponse;
 import org.devcourse.resumeme.business.resume.domain.ReferenceLink;
 import org.devcourse.resumeme.business.resume.entity.Component;
 
 @Data
+@NoArgsConstructor
+@JsonTypeName("links")
 public class ResumeLinkResponse extends ComponentResponse {
 
     private String linkType;
     private String url;
 
-    public ResumeLinkResponse(ReferenceLink referenceLink, Component component) {
-        super(component);
+    public ResumeLinkResponse(String type, ReferenceLink referenceLink, Component component) {
+        super(type, component);
         this.linkType = referenceLink.getLinkType().name();
         this.url = referenceLink.getAddress();
     }
