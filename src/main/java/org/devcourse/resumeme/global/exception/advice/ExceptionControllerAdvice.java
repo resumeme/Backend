8f5cfa1fb7d4e89170491b2e1209dc8c40310866
ddongpuri <96704446+ddongpuri@users.noreply.github.com>
2023-11-20 17:41:37 +0900

    엔드포인트 수정 및 자료구조 변경 (#237)

--- src/main/java/org/devcourse/resumeme/global/exception/advice/ExceptionControllerAdvice.java
+++ src/main/java/org/devcourse/resumeme/global/exception/advice/ExceptionControllerAdvice.java
@@ -1,6 +1,7 @@
 package org.devcourse.resumeme.global.exception.advice;
 
 import org.devcourse.resumeme.global.exception.CustomException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,6 +23,12 @@ public class ExceptionControllerAdvice {
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ErrorResponse httpRequestMethodNotSupportedExceptionHandle(HttpRequestMethodNotSupportedException exception) {
         return new ErrorResponse("endpoint, method를 다시 확인해주세요", "NOT_MATCH_METHOD_ENDPOINT");
+    }
+
+    @ResponseStatus(METHOD_NOT_ALLOWED)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ErrorResponse httpRequestMethodNotSupportedExceptionHandle(HttpMessageNotReadableException exception) {
+        return new ErrorResponse("Request 데이터가 올바르지 않습니다.", "NOT_MATCH_REQUEST_BODY");
     }
 
 }
