65d3ccc06688381e6574e71789fbae750aebf5a3
JHS <wnghdtjr129@naver.com>
2023-11-21 18:21:49 +0900

    feat: 반환값 이벤트 아이디 추가

--- src/main/java/org/devcourse/resumeme/business/userevent/controller/dto/MenteeEventResponse.java
+++ src/main/java/org/devcourse/resumeme/business/userevent/controller/dto/MenteeEventResponse.java
@@ -5,11 +5,11 @@ import org.devcourse.resumeme.business.event.domain.MenteeToEvent;
 
 import java.time.LocalDateTime;
 
-public record MenteeEventResponse(Long resumeId, String status, String title,
+public record MenteeEventResponse(Long eventId, Long resumeId, String status, String title,
                                   String mentorName, LocalDateTime startDate, LocalDateTime endDate) {
 
     public MenteeEventResponse(MenteeToEvent applicant, Event event) {
-        this(applicant.getResumeId(), applicant.getProgress().name(), event.getEventInfo().getTitle(),
+        this(event.getId(), applicant.getResumeId(), applicant.getProgress().name(), event.getEventInfo().getTitle(),
                 event.getMentor().getRequiredInfo().getRealName(), event.getEventTimeInfo().getOpenDateTime(), event.getEventTimeInfo().getCloseDateTime());
     }
 
