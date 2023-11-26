e0f296f441575f12a8973c08f968c07aadfd18be
JuHongseok <wnghdtjr129@naver.com>
2023-11-26 22:59:42 +0900

    feat: 첨삭 완료 시 피드백 스냅샷 제공

--- src/main/java/org/devcourse/resumeme/business/resume/controller/SnapshotController.java
+++ src/main/java/org/devcourse/resumeme/business/resume/controller/SnapshotController.java
@@ -4,7 +4,11 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
+import org.devcourse.resumeme.business.comment.controller.dto.CommentWithReviewResponse;
 import org.devcourse.resumeme.business.resume.controller.career.dto.ComponentResponse;
+import org.devcourse.resumeme.business.resume.controller.dto.CommentSnapshotResponse;
+import org.devcourse.resumeme.business.resume.controller.dto.ResumeSnapshotResponse;
+import org.devcourse.resumeme.business.resume.controller.dto.SnapshotResponse;
 import org.devcourse.resumeme.business.resume.entity.Snapshot;
 import org.devcourse.resumeme.business.resume.service.SnapshotService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,16 +28,23 @@ public class SnapshotController {
     private final SnapshotService snapshotService;
 
     @GetMapping
-    public Map<String, List<ComponentResponse>> getSnapshot(@RequestParam Long resumeId) throws IOException {
+    public SnapshotResponse getSnapshot(@RequestParam Long resumeId, @RequestParam String type) throws IOException {
         Snapshot snapshot = snapshotService.getByResumeId(resumeId);
 
-        String resumeData = snapshot.getResumeData();
+        String data = snapshot.get(type);
 
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
-        Map<String, List<ComponentResponse>> response = objectMapper.readValue(resumeData, new TypeReference<>() {});
 
-        return response;
+        if (type.equals("resume")) {
+            Map<String, List<ComponentResponse>> response = objectMapper.readValue(data, new TypeReference<>() {});
+
+            return new ResumeSnapshotResponse(response);
+        }
+
+        CommentWithReviewResponse response = objectMapper.readValue(data, new TypeReference<>() {});
+
+        return new CommentSnapshotResponse(response);
     }
 
 }
