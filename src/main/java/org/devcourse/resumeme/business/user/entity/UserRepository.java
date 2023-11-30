19af7187ba9b97c8b0d4c136459b8f2df72649ee
JuHongseok <75611167+Juhongseok@users.noreply.github.com>
2023-11-30 23:59:54 +0900

    position, fields left join 추가 (#330)

--- src/main/java/org/devcourse/resumeme/business/user/entity/UserRepository.java
+++ src/main/java/org/devcourse/resumeme/business/user/entity/UserRepository.java
@@ -14,7 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u from User u left join fetch u.userPositions left join fetch u.interestedFields where u.id = :userId")
     Optional<User> findWithPositionsAndFields(@Param("userId") Long userId);
 
-    @Query("select u from User u join fetch u.userPositions where u.id in :ids")
+    @Query("select u from User u left join fetch u.userPositions left join fetch u.interestedFields where u.id in :ids")
     List<User> findAllByIds(@Param("ids") List<Long> ids);
 
 }
