e6582c4122733e3d27a9e0f8fb63fb48121c0489
JuHongseok <75611167+Juhongseok@users.noreply.github.com>
2023-12-02 20:04:32 +0900

    피드백을 하나도 안달았을 경우 이력서 스냅샷 찍어오기 추가 (#347)

--- src/main/java/org/devcourse/resumeme/business/snapshot/service/CommentCapture.java
+++ src/main/java/org/devcourse/resumeme/business/snapshot/service/CommentCapture.java
@@ -2,6 +2,7 @@ package org.devcourse.resumeme.business.snapshot.service;
 
 import lombok.RequiredArgsConstructor;
 import org.devcourse.resumeme.business.comment.controller.dto.CommentWithReviewResponse;
+import org.devcourse.resumeme.business.resume.controller.dto.AllComponentResponse;
 import org.devcourse.resumeme.business.snapshot.entity.Snapshot;
 import org.devcourse.resumeme.business.snapshot.repository.SnapshotRepository;
 import org.devcourse.resumeme.common.util.ObjectMapperUtils;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+
+import static org.devcourse.resumeme.common.util.ObjectMapperUtils.getData;
+import static org.devcourse.resumeme.common.util.RestTemplateUtils.getLocalResponse;
 
 @Service
 @Transactional
@@ -36,8 +40,9 @@ public class CommentCapture implements SnapshotCapture {
             return;
         }
 
-        snapshotRepository.save(new Snapshot(null, data, resumeId));
-
+        AllComponentResponse componentResponse = getLocalResponse("/api/v1/resumes/" + resumeId, AllComponentResponse.class);
+        String resumeData = getData(componentResponse);
+        snapshotRepository.save(new Snapshot(resumeData, data, resumeId));
     }
 
 }
