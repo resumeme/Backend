cae8421e921b30cc2e54ff00151caa95362fc942
JuHongseok <wnghdtjr129@naver.com>
2023-12-16 13:55:25 +0900

    chore: 라인 import 정리

--- src/main/java/org/devcourse/resumeme/business/event/service/ParticipantProgressChanger.java
+++ src/main/java/org/devcourse/resumeme/business/event/service/ParticipantProgressChanger.java
@@ -3,12 +3,7 @@ package org.devcourse.resumeme.business.event.service;
 import lombok.RequiredArgsConstructor;
 import org.devcourse.resumeme.business.event.domain.MenteeToEvent;
 import org.devcourse.resumeme.business.event.repository.MenteeToEventRepository;
-import org.devcourse.resumeme.global.exception.CustomException;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-import static org.devcourse.resumeme.global.exception.ExceptionCode.RESUME_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -18,9 +13,7 @@ public class ParticipantProgressChanger implements ParticipantProvider {
 
     @Override
     public void finishProgress(Long menteeId, Long resumeId) {
-        List<MenteeToEvent> participants = menteeToEventRepository.findByMenteeId(menteeId);
-
-        participants.stream()
+        menteeToEventRepository.findByMenteeId(menteeId).stream()
                 .filter(participant -> participant.isSameResume(resumeId))
                 .findFirst()
                 .ifPresent(MenteeToEvent::editComplete);
