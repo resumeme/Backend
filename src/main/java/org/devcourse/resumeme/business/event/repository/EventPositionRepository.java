f75954b964d4108b4e922f3d59c501d4c75e667b
JuHongseok <wnghdtjr129@naver.com>
2023-12-16 13:43:52 +0900

    refactor: 조회 메소드 통합

--- src/main/java/org/devcourse/resumeme/business/event/repository/EventPositionRepository.java
+++ src/main/java/org/devcourse/resumeme/business/event/repository/EventPositionRepository.java
@@ -9,9 +9,6 @@ import java.util.List;
 
 public interface EventPositionRepository extends JpaRepository<EventPosition, Long> {
 
-    @Query("select ep from EventPosition ep where ep.event.id = :eventId")
-    List<EventPosition> findAllByEventId(@Param("eventId") Long eventId);
-
     @Query("select ep from EventPosition ep where ep.event.id in :eventIds")
     List<EventPosition> findAllByEventIds(@Param("eventIds") List<Long> eventIds);
 
